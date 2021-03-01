cd57579f6b7567d08a385128e8a521987eaac0e3
Denis Ryabov <dryabov@yandex.ru>
2021-03-02 01:22:39 +0300

    remove notice on unused argnum syntax, but keep detection of incorrect argnum syntax

--- administrator/components/com_jedchecker/libraries/rules/language.php
+++ administrator/components/com_jedchecker/libraries/rules/language.php
@@ -217,16 +217,28 @@ class JedcheckerRulesLanguage extends JEDcheckerRule
 				$this->report->addWarning($file, JText::_('COM_JEDCHECKER_LANG_VARIABLE_REF'), $startLineno, $line);
 			}
 
-			// Count %... formats in the string
+			// The code below detects incorrect format of numbered placeholders (e.g. "%1s" instead of "%1$s")
+
+			// Count all placeholders in the string
 			$countAll = preg_match_all('/(?<=^|[^%])%(?=[-+0 ]?\w)/', $value);
 
-			// Count %n$... (argnum) formats in the string
-			$countArgnum = preg_match_all('/(?<=^|[^%])%\d+\$/', $value);
+			// Count numbered placeholders in the string (e.g. "%1s")
+			$count = preg_match_all('/(?<=^|[^%])%(\d+)\w/', $value, $matches);
 
-			if ($countAll > 1 && $countArgnum < $countAll)
+			if ($count === $countAll && $count > 1)
 			{
-				// @todo It's not mentioned in docs
-				$this->report->addInfo($file, JText::_('COM_JEDCHECKER_LANG_RECOMMEND_ARGNUM'), $startLineno, $line);
+				$maxNumber = 0;
+
+				foreach ($matches as $match)
+				{
+					$maxNumber = max($maxNumber, (int) $match[1]);
+				}
+
+				// If placeholder numbers form a sequence, the maximal value is equal to the number of elements
+				if ($maxNumber === $count)
+				{
+					$this->report->addWarning($file, JText::_('COM_JEDCHECKER_LANG_INCORRECT_ARGNUM'), $startLineno, $line);
+				}
 			}
 		}
 
